65d41cc208a3634982cce3dd89a89217ba4d9997
Nitish Namdeo <namdeonitish@gmail.com>
2024-10-01 19:37:33 +0530

    O(m+n)

--- Stack_n_Queue/Next_Greater_Element_I.py
+++ Stack_n_Queue/Next_Greater_Element_I.py
@@ -1,6 +1,23 @@
 class Solution:
     def nextGreaterElement(self, nums1: list[int], nums2: list[int]) -> list[int]:
-        # O(m*n)
+        # TC: O(m+n), SC: O(m)
+        
+        nums1Idx = { n:i for i, n in enumerate(nums1)}
+        res = [-1] * len(nums1)
+
+        stack = []
+
+        for i in range(len(nums2)):
+            cur = nums2[i]
+            while stack and cur > stack[-1]:
+                val = stack.pop()
+                idx = nums1Idx[val]
+                res[idx] = cur
+            if cur in nums1Idx:
+                stack.append(cur)
+        return res
+            
+        # TC: O(m*n), SC: O(m)
 
         nums1Idx = {n:i for i, n in enumerate(nums1)} # hashmap
         res = [-1] * len(nums1) # output
