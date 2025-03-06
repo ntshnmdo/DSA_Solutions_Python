780fa199acb36562a16f731e8eb27fae765093dd
Nitish Namdeo <namdeonitish@gmail.com>
2025-03-06 18:13:08 +0530

    Update_1_LB_Array

--- Array_String_Hashing/1_reverse_array_string.py
+++ Array_String_Hashing/1_reverse_array_string.py
@@ -9,3 +9,23 @@ print(A)
 reverseList(A, 0, 5)
 print("Reversed list is ")
 print(A)
+
+#1 Using  Slicing  TC: O(n), SC: O(n) because a new list is created
+
+def reverse_array(arr):
+    return arr[::-1]
+
+#2 Using Two-Pointer Approach (efficient, in-phase), TC: O(n), SC:O(1) modifies original list
+
+def reverse_array(arr):
+    left, right = 0, len(arr)-1
+    while left<right:
+        arr[left], arr[right] = arr[right], arr[left] # swap element
+        left += 1
+        right -= 1
+
+reverse_array(arr) 
+
+#3 Using Python built in reverse() method (modifies in place), TC: O(n) SC: O(1)
+
+arr.reverse()
